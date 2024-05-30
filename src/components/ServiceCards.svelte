9ccdba81796b9fa4b42e11c66078ae749c90343e
barraIhsan <57800056+barraIhsan@users.noreply.github.com>
2024-05-31 06:18:04 +0700

    feat: add services section on homepage
    TODO: Put the data in a seperate .js file and generate the element based
    on the data instead

---
+++ src/components/ServiceCards.svelte
@@ -0,0 +1,10 @@
+<script>
+	export let imgSrc, imgAlt, title, desc, btnText;
+</script>
+
+<div class="flex flex-col gap-5 text-dark bg-white p-4 rounded-2xl">
+	<img src="{imgSrc}" alt="{imgAlt}" class="rounded-2xl h-80 w-fit object-cover">
+	<h3 class="font-semibold text-3xl">{title}</h3>
+	<p class="text-xl">{desc}</p>
+	<a href="#" class="py-6 bg-[#95BB6F] hover:bg-[#7a995b] text-white text-center font-semibold rounded-full transition">{btnText}</a>
+</div>
