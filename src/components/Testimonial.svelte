47fbf2e21e547b3668279dc1732c6fc40b3ef5f7
barraIhsan <57800056+barraIhsan@users.noreply.github.com>
2024-05-31 06:25:57 +0700

    feat: add testimonial section on homepage
    TODO:

---
+++ src/components/Testimonial.svelte
@@ -0,0 +1,9 @@
+<script>
+	export let quote, author;
+</script>
+
+<div class="bg-white p-8 text-dark text-lg flex flex-col gap-5 rounded-2xl">
+	<img src="/img/quote.svg" alt="Quote" class="size-fit">
+	<p>{quote}</p>
+	<p class="font-bold list-item list-inside">{author}</p>
+</div>
