f38a8aad69a362f9896778f439f5008b6df2eb14
barraIhsan <57800056+barraIhsan@users.noreply.github.com>
2024-05-31 06:11:09 +0700

    feat: initial commit
    Initialize Astro project with tailwind and svelte, and added some colors
    and font to the tailwind configuration that we can use easily

---
+++ tailwind.config.mjs
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ["Barlow", "Arial", "sans-serif"],
+			},
+			borderWidth: {
+				10: "10px",
+			},
+			colors: {
+				dark: "#2E5070",
+				light: "#EDF8F9",
+				green: "#95BB6F",
+				gold: "#F6C956",
+				fuschia: "#C6EAE3",
+			},
+		},
+	},
+	plugins: [],
+};
