588e0e252daa3bccd7fc99e0002b8e071f6727b6
barraIhsan <57800056+barraIhsan@users.noreply.github.com>
2024-05-31 07:26:23 +0700

    feat: make element in phone screen slightly bigger
    Utilize the xs breakpoint, now it will make the responsiveness better

--- src/components/Info.svelte
+++ src/components/Info.svelte
@@ -2,7 +2,7 @@
 	export let title, desc;
 </script>
 
-<div class="text-base sm:text-lg text-dark">
+<div class="text-base xs:text-lg text-dark">
 	<h4 class="font-bold">{title}</h4>
 	<p>{desc}</p>
 </div>
--- src/components/Testimonial.svelte
+++ src/components/Testimonial.svelte
@@ -2,7 +2,7 @@
 	export let quote, author;
 </script>
 
-<div class="bg-white p-8 text-dark text-base sm:text-lg flex flex-col gap-5 rounded-2xl">
+<div class="bg-white p-8 text-dark text-base xs:text-lg flex flex-col gap-5 rounded-2xl">
 	<img src="/img/quote.svg" alt="Quote" class="w-8 sm:w-fit">
 	<p>{quote}</p>
 	<p class="font-bold list-item list-inside">{author}</p>
