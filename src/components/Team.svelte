5031d3497073b5aacf927a785177a26ccee2a0c1
barraIhsan <57800056+barraIhsan@users.noreply.github.com>
2024-05-31 20:52:23 +0700

    feat: make the about page responsive

--- src/components/Team.svelte
+++ src/components/Team.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div class="flex flex-col gap-3 text-dark">
-	<img src="/img/icon-staff.png" alt="Staff Profile" class="rounded-2xl h-80 object-cover">
-	<p class="text-xl">{role}</p>
-	<p class="text-3xl font-bold">{name}</p>
+	<img src="/img/icon-staff.png" alt="Staff Profile" class="rounded-2xl h-60 xs:h-80 object-cover">
+	<p class="text-lg xs:text-xl">{role}</p>
+	<p class="text-2xl xs:text-3xl font-bold">{name}</p>
 </div>
--- src/components/Title.svelte
+++ src/components/Title.svelte
@@ -2,8 +2,8 @@
 	export let title, desc;
 </script>
 
-<div class="flex gap-5 px-5">
-	<h1 class="text-dark font-black text-6xl">{title}</h1>
+<div class="flex flex-col xl:flex-row gap-10 xl:gap-5 px-5">
+	<h1 class="text-dark font-black text-5xl xs:text-6xl">{title}</h1>
 	<img src="/img/paw-hero.svg" alt="Cat Paw" class="w-36 md:w-52 mr-20">
-	<p class="text-xl">{desc}</p>
+	<p class="text-lg xs:text-xl">{desc}</p>
 </div>
