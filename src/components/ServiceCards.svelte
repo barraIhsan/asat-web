3d920bc724a09de766cb81d7dbf28c06ec831fa1
barraIhsan <57800056+barraIhsan@users.noreply.github.com>
2024-05-31 07:04:08 +0700

    feat: make the services section responsive
    Services section on homepage

--- src/components/ServiceCards.svelte
+++ src/components/ServiceCards.svelte
@@ -4,7 +4,7 @@
 
 <div class="flex flex-col gap-5 text-dark bg-white p-4 rounded-2xl">
 	<img src="{imgSrc}" alt="{imgAlt}" class="rounded-2xl h-80 w-fit object-cover">
-	<h3 class="font-semibold text-3xl">{title}</h3>
-	<p class="text-xl">{desc}</p>
+	<h3 class="font-semibold text-2xl sm:text-3xl">{title}</h3>
+	<p class="text-lg sm:text-xl">{desc}</p>
 	<a href="#" class="py-6 bg-[#95BB6F] hover:bg-[#7a995b] text-white text-center font-semibold rounded-full transition">{btnText}</a>
 </div>
