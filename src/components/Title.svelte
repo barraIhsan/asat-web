5e3bf68a6d4123d6d73d26e7257743624ae4c49f
barraIhsan <57800056+barraIhsan@users.noreply.github.com>
2024-05-31 21:47:11 +0700

    feat: make title component

--- src/components/Title.svelte
+++ src/components/Title.svelte
@@ -2,7 +2,7 @@
 	export let title, desc;
 </script>
 
-<div class="flex flex-col xl:flex-row gap-10 xl:gap-5">
+<div class="flex flex-col xl:flex-row items-center gap-10 xl:gap-5">
 	<h1 class="text-dark font-black text-5xl xs:text-6xl">{title}</h1>
 	<img src="/img/paw-hero.svg" alt="Cat Paw" class="w-36 md:w-52 mr-20">
 	<p class="text-lg xs:text-xl">{desc}</p>
