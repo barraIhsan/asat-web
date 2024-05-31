ead8ea94a679e5ea3680109330a754c3e51ce608
barraIhsan <57800056+barraIhsan@users.noreply.github.com>
2024-05-31 22:21:06 +0700

    feat: make

--- src/components/ClinicFeatures.svelte
+++ src/components/ClinicFeatures.svelte
@@ -2,7 +2,7 @@
 	export let title, desc;
 </script>
 
-<div class="flex gap-3 py-10">
-	<h2 class="text-dark font-bold text-4xl w-1/2">{title}</h2>
-	<p class="text-xl w-1/2">{desc}</p>
+<div class="flex flex-col lg:flex-row gap-5 lg:py-10">
+	<h2 class="text-dark font-bold text-4xl lg:w-1/2">{title}</h2>
+	<p class="text-xl lg:w-1/2">{desc}</p>
 </div>
--- src/components/ClinicSchedule.svelte
+++ src/components/ClinicSchedule.svelte
@@ -2,8 +2,8 @@
 	export let imgSrc, name, day, time
 </script>
 
-<div class="flex gap-3 text-dark w-full">
-	<img src="{imgSrc}" alt="{name}" class="size-72 rounded-full object-cover">
+<div class="flex flex-col sm:flex-row gap-3 text-dark text-center sm:text-left w-fit sm:w-full">
+	<img src="{imgSrc}" alt="{name}" class="size-72 sm:size-48 xl:size-56 2xl:size-72 rounded-full object-cover">
 	<div class="flex flex-col gap-6 w-full">
 		<p class="font-bold text-3xl">{name}</p>
 		<div class="border-y border-fuschia py-6">
