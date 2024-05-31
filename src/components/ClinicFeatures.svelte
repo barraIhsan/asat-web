a8d0264ecf6ec0f229157965c8db0a96152abeb5
barraIhsan <57800056+barraIhsan@users.noreply.github.com>
2024-05-31 21:47:48 +0700

    feat: add clinic page
    TODO: make it responsive

---
+++ src/components/ClinicFeatures.svelte
@@ -0,0 +1,8 @@
+<script>
+	export let title, desc;
+</script>
+
+<div class="flex gap-3 py-10">
+	<h2 class="text-dark font-bold text-4xl w-1/2">{title}</h2>
+	<p class="text-xl w-1/2">{desc}</p>
+</div>
---
+++ src/components/ClinicSchedule.svelte
@@ -0,0 +1,14 @@
+<script>
+	export let imgSrc, name, day, time
+</script>
+
+<div class="flex gap-3 text-dark w-full">
+	<img src="{imgSrc}" alt="{name}" class="size-72 rounded-full object-cover">
+	<div class="flex flex-col gap-6 w-full">
+		<p class="font-bold text-3xl">{name}</p>
+		<div class="border-y border-fuschia py-6">
+			<p class="text-xl">{day}</p>
+			<p class="text-xl">{time}</p>
+		</div>
+	</div>
+</div>
