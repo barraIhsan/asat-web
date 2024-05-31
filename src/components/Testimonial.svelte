7f061da83d422800c9e97ccb180fbf0298ca4c71
barraIhsan <57800056+barraIhsan@users.noreply.github.com>
2024-05-31 07:17:49 +0700

    fix: make the testimonial section more responsive
    Testimonial section on homepage

--- src/components/Testimonial.svelte
+++ src/components/Testimonial.svelte
@@ -2,8 +2,8 @@
 	export let quote, author;
 </script>
 
-<div class="bg-white p-8 text-dark text-lg flex flex-col gap-5 rounded-2xl">
-	<img src="/img/quote.svg" alt="Quote" class="size-fit">
+<div class="bg-white p-8 text-dark text-base sm:text-lg flex flex-col gap-5 rounded-2xl">
+	<img src="/img/quote.svg" alt="Quote" class="w-8 sm:w-fit">
 	<p>{quote}</p>
 	<p class="font-bold list-item list-inside">{author}</p>
 </div>
