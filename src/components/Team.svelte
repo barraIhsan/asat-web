126dee45d475ae5d9b756455618dd3b225c0206a
barraIhsan <57800056+barraIhsan@users.noreply.github.com>
2024-05-31 19:51:07 +0700

    feat: add about page
    TODO: make it responsive

---
+++ src/components/Team.svelte
@@ -0,0 +1,9 @@
+<script>
+	export let role, name;
+</script>
+
+<div class="flex flex-col gap-3 text-dark">
+	<img src="/img/icon-staff.png" alt="Staff Profile" class="rounded-2xl h-80 object-cover">
+	<p class="text-xl">{role}</p>
+	<p class="text-3xl font-bold">{name}</p>
+</div>
---
+++ src/components/Title.svelte
@@ -0,0 +1,9 @@
+<script>
+	export let title, desc;
+</script>
+
+<div class="flex gap-5 px-5">
+	<h1 class="text-dark font-black text-6xl">{title}</h1>
+	<img src="/img/paw-hero.svg" alt="Cat Paw" class="w-36 md:w-52 mr-20">
+	<p class="text-xl">{desc}</p>
+</div>
