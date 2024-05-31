4c7016b66bea5eb4436f594aab1229477fc5c5f7
barraIhsan <57800056+barraIhsan@users.noreply.github.com>
2024-06-01 06:40:22 +0700

    style: use color variable on ServiceCards

--- src/components/ServiceCards.svelte
+++ src/components/ServiceCards.svelte
@@ -6,5 +6,5 @@
 	<img src="{imgSrc}" alt="{imgAlt}" class="rounded-2xl h-80 w-fit object-cover">
 	<h3 class="font-semibold text-2xl xs:text-3xl">{title}</h3>
 	<p class="text-lg xs:text-xl">{desc}</p>
-	<a href="{link}" class="py-6 bg-[#95BB6F] hover:bg-[#7a995b] text-white text-center font-semibold rounded-full transition">{btnText}</a>
+	<a href="{link}" class="py-6 bg-green hover:bg-greenHover text-white text-center font-semibold rounded-full transition">{btnText}</a>
 </div>
