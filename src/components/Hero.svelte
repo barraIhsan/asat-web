c86e76a1138834a90ff07b10c5143b037354874d
barraIhsan <57800056+barraIhsan@users.noreply.github.com>
2024-06-01 08:01:50 +0700

    feat: add Hero components
    Because all the pages are using the same layout, I decided to make Hero
    component to make this codebase more DRY.
    
    It also utilize `<slot>` element so we can insert content inside our
    svelte component

---
+++ src/components/Hero.svelte
@@ -0,0 +1,13 @@
+<script>
+	import Title from './Title.svelte'
+
+	export let imgSrc, imgAlt, title, desc;
+</script>
+
+<img src="{imgSrc}" alt={imgAlt} class="h-56 xs:h-96 xl:h-[600px] w-full object-cover">
+<section class="bg-light flex flex-col gap-20 py-20 px-6 sm:px-12 md:px-24 xl:px-44 2xl:px-80">
+	<Title title={title} desc={desc} />
+
+	<slot />
+
+</section>
