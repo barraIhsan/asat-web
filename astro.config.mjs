ade37950a7de3d9da7632ad1492b2a268cdadd86
barraIhsan <57800056+barraIhsan@users.noreply.github.com>
2024-06-01 15:29:38 +0700

    fix: use astro config file instead of vite

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,12 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()]
+	integrations: [tailwind(), svelte()],
+	vite: {
+		build: {
+			rollupOptions: {
+				external: ["@fontsource/Barlow"],
+			},
+		},
+	},
 });
