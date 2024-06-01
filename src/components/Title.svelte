c2b53cfeb5a7ff3ac347da23235bcdddb45812a8
barraIhsan <57800056+barraIhsan@users.noreply.github.com>
2024-06-01 07:21:41 +0700

    feat: add min-width in Title to handle long titl

--- src/components/Title.svelte
+++ src/components/Title.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div class="flex flex-col xl:flex-row xl:items-center gap-10 xl:gap-5">
-	<h1 class="text-dark font-black text-5xl xs:text-6xl">{title}</h1>
+	<h1 class="text-dark font-black text-5xl xs:text-6xl min-w-72">{title}</h1>
 	<img src="/img/paw-hero.svg" alt="Cat Paw" class="w-36 md:w-52 mr-20">
 	<p class="text-lg xs:text-xl">{desc}</p>
 </div>
