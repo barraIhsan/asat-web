2f515d68d4d62459843fa164aebd5a0bc912986f
barraIhsan <57800056+barraIhsan@users.noreply.github.com>
2024-06-02 12:53:14 +0700

    feat: make the clinic feature not hardcoded text

--- src/components/ClinicFeatures.svelte
+++ src/components/ClinicFeatures.svelte
@@ -1,8 +1,10 @@
 <script>
-	export let title, desc;
+	export let features;
 </script>
 
-<div class="flex flex-col lg:flex-row gap-5 lg:py-10">
-	<h2 class="text-dark font-bold text-4xl lg:w-1/2">{title}</h2>
-	<p class="text-xl lg:w-1/2">{desc}</p>
-</div>
+{#each features as feature}
+	<div class="flex flex-col lg:flex-row gap-5 lg:py-10">
+		<h2 class="text-dark font-bold text-4xl lg:w-1/2">{feature.title}</h2>
+		<p class="text-xl lg:w-1/2">{feature.desc}</p>
+	</div>
+{/each}
---
+++ src/data/clinicFeatures.js
@@ -0,0 +1,26 @@
+export const clinicFeatures = [
+	{
+		title: "1. Pemeriksaan Kesehatan Rutin",
+		desc: "Kami menawarkan pemeriksaan kesehatan rutin untuk memastikan kucing Anda dalam kondisi prima. Pemeriksaan ini meliputi pengecekan fisik, pengukuran berat badan, dan evaluasi kesehatan secara keseluruhan",
+	},
+	{
+		title: "2. Vaksinasi",
+		desc: "Lindungi kucing Anda dari penyakit berbahaya dengan vaksinasi yang tepat waktu. Kami menyediakan vaksinasi lengkap sesuai dengan jadwal yang direkomendasikan oleh dokter hewan",
+	},
+	{
+		title: "3. Pengobatan Penyakit",
+		desc: "Apapun penyakit yang mungkin dihadapi kucing Anda, kami siap memberikan diagnosis yang akurat dan pengobatan yang efektif. Tim kami berkomitmen untuk memberikan perawatan terbaik agar kucing Anda segera pulih",
+	},
+	{
+		title: "4. Bedah",
+		desc: "Klinik kami dilengkapi dengan ruang operasi yang steril dan peralatan modern untuk melakukan berbagai prosedur bedah, dari operasi minor hingga operasi mayor. Dokter hewan kami akan memberikan penjelasan mendetail dan dukungan pasca-operasi",
+	},
+	{
+		title: "5. Konsultasi Kesehatan",
+		desc: "Punya pertanyaan tentang kesehatan kucing Anda? Kami menyediakan layanan konsultasi kesehatan untuk memberikan informasi dan saran yang Anda butuhkan. Baik secara langsung di klinik atau melalui konsultasi online",
+	},
+	{
+		title: "6. Laboratorium Diagnostik",
+		desc: "Dengan fasilitas laboratorium yang lengkap, kami dapat melakukan berbagai tes diagnostik seperti tes darah, urin, dan lainnya untuk mendukung diagnosis yang akurat",
+	}
+];
