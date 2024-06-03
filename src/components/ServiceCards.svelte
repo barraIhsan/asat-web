828fc8222b60164322eceac68a95a98a893e8822
barraIhsan <57800056+barraIhsan@users.noreply.github.com>
2024-06-03 21:37:24 +0700

    feat: make the service fetches from js

--- src/components/ServiceCards.svelte
+++ src/components/ServiceCards.svelte
@@ -1,10 +1,13 @@
 <script>
-	export let imgSrc, imgAlt, title, desc, link, btnText;
+	export let serviceData;
 </script>
-
-<div class="flex-1 flex flex-col gap-5 text-dark bg-white p-4 rounded-2xl">
-	<img src="{imgSrc}" alt="{imgAlt}" class="rounded-2xl h-80 object-cover">
-	<h3 class="font-semibold text-2xl xs:text-3xl">{title}</h3>
-	<p class="text-lg xs:text-xl lg:mb-14">{desc}</p>
-	<a href="{link}" class="py-6 bg-green hover:bg-greenHover text-white text-center font-semibold rounded-full transition mt-auto">{btnText}</a>
+<div class="flex flex-col lg:flex-row gap-8 m-5 sm:m-10">
+	{#each serviceData as service}
+		<div class="flex-1 flex flex-col gap-5 text-dark bg-white p-4 rounded-2xl">
+			<img src="{service.imgSrc}" alt="{service.imgAlt}" class="rounded-2xl h-80 object-cover">
+			<h3 class="font-semibold text-2xl xs:text-3xl">{service.title}</h3>
+			<p class="text-lg xs:text-xl lg:mb-14">{service.desc}</p>
+			<a href="{service.link}" class="py-6 bg-green hover:bg-greenHover text-white text-center font-semibold rounded-full transition mt-auto">{service.btnText}</a>
+		</div>
+	{/each}
 </div>
---
+++ src/data/serviceData.js
@@ -0,0 +1,26 @@
+export let serviceData = [
+	{
+		imgSrc: "/img/service-klinik.jpeg",
+		imgAlt: "Cat's Clinic Service",
+		title: "Klinik Kucing",
+		desc: "Tim dokter hewan kami yang berpengalaman siap memberikan perawatan medis terbaik untuk kucing Anda. Kami menyediakan layanan vaksinasi, pemeriksaan rutin, pengobatan penyakit, dan konsultasi kesehatan.",
+		link: "/clinic",
+		btnText: "Cek Dokter",
+	},
+	{
+		imgSrc: "/img/service-daycare.webp",
+		imgAlt: "Cat's Daycare Service",
+		title: "Daycare & Grooming",
+		desc: "Butuh tempat yang aman dan nyaman untuk menitipkan kucing Anda saat Anda sibuk? Daycare kami adalah solusinya. Selain itu, layanan grooming kami akan membuat kucing Anda tetap bersih dan cantik.",
+		link: "/daycare",
+		btnText: "Lihat Paket",
+	},
+	{
+		imgSrc: "/img/service-adopsi.jpeg",
+		imgAlt: "Cat's Adoption Service",
+		title: "Adopsi Kucing",
+		desc: "Bantu kami menemukan rumah baru yang penuh cinta untuk kucing-kucing yang membutuhkan. Lihat kucing-kucing yang siap diadopsi dan jadilah bagian dari cerita bahagia mereka.",
+		link: "/adoption",
+		btnText: "Cari Kucing"
+	}
+];
