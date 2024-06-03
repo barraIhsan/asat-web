e3d1de5e40bc361b523c4694e5648b6e9314f32f
barraIhsan <57800056+barraIhsan@users.noreply.github.com>
2024-06-03 15:01:06 +0700

    feat: fetch the doctors data from js

--- src/components/ClinicSchedule.svelte
+++ src/components/ClinicSchedule.svelte
@@ -1,14 +1,18 @@
 <script>
-	export let imgSrc, name, day, time
+	export let doctorData;
 </script>
 
-<div class="flex flex-col sm:flex-row gap-3 text-dark text-center sm:text-left w-fit sm:w-full">
-	<img src="{imgSrc}" alt="{name}" class="size-72 sm:size-48 xl:size-56 2xl:size-72 rounded-full object-cover">
-	<div class="flex flex-col gap-6 w-full">
-		<p class="font-bold text-3xl">{name}</p>
-		<div class="border-y border-fuschia py-6">
-			<p class="text-xl">{day}</p>
-			<p class="text-xl">{time}</p>
+<div class="grid grid-cols-1 lg:grid-cols-2 gap-20">
+	{#each doctorData as doctor}
+		<div class="flex flex-col sm:flex-row gap-3 text-dark text-center sm:text-left w-fit sm:w-full">
+			<img src="{doctor.imgSrc}" alt="{doctor.name}" class="size-72 sm:size-48 xl:size-56 2xl:size-72 rounded-full object-cover">
+			<div class="flex flex-col gap-6 w-full">
+				<p class="font-bold text-3xl">{doctor.name}</p>
+				<div class="border-y border-fuschia py-6">
+					<p class="text-xl">{doctor.day}</p>
+					<p class="text-xl">{doctor.time}</p>
+				</div>
+			</div>
 		</div>
-	</div>
+	{/each}
 </div>
---
+++ src/data/doctorData.js
@@ -0,0 +1,14 @@
+export const doctorData = [
+	{
+		imgSrc: "/img/dokter-bima-sakti.jpeg",
+		name: "dr. Bima Sakti",
+		day: "Senin, Rabu, Jumat",
+		time: "08.00 - 15.00",
+	},
+	{
+		imgSrc: "/img/dokter-andromeda.jpeg",
+		name: "dr. Andromeda",
+		day: "Selasa, Kamis, Sabtu",
+		time: "10.00 - 17.00"
+	},
+];
