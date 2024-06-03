e841b4e10a6ee8bdc3f776460df746a881cb3478
barraIhsan <57800056+barraIhsan@users.noreply.github.com>
2024-06-03 15:17:24 +0700

    feat: use `flex-1` because the parent is a flex

--- src/components/ClinicFeatures.svelte
+++ src/components/ClinicFeatures.svelte
@@ -5,8 +5,8 @@
 <ol class="space-y-10">
 	{#each features as feature}
 		<li class="flex flex-col lg:flex-row gap-5 lg:py-10">
-			<h2 class="text-dark font-bold text-4xl lg:w-1/2">{feature.title}</h2>
-			<p class="text-xl lg:w-1/2">{feature.desc}</p>
+			<h2 class="text-dark font-bold text-4xl lg:flex-1">{feature.title}</h2>
+			<p class="text-xl lg:flex-1">{feature.desc}</p>
 		</li>
 	{/each}
 </ol>
