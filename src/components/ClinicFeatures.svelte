e3adfa73ec9c3619ea8a95b230255684738f7f54
barraIhsan <57800056+barraIhsan@users.noreply.github.com>
2024-06-03 14:31:24 +0700

    fix: use `ol`

--- src/components/ClinicFeatures.svelte
+++ src/components/ClinicFeatures.svelte
@@ -2,9 +2,11 @@
 	export let features;
 </script>
 
-{#each features as feature}
-	<div class="flex flex-col lg:flex-row gap-5 lg:py-10">
-		<h2 class="text-dark font-bold text-4xl lg:w-1/2">{feature.title}</h2>
-		<p class="text-xl lg:w-1/2">{feature.desc}</p>
-	</div>
-{/each}
+<ol class="space-y-10">
+	{#each features as feature}
+		<li class="flex flex-col lg:flex-row gap-5 lg:py-10">
+			<h2 class="text-dark font-bold text-4xl lg:w-1/2">{feature.title}</h2>
+			<p class="text-xl lg:w-1/2">{feature.desc}</p>
+		</li>
+	{/each}
+</ol>
