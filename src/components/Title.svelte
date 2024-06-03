2325b196b5466a7d29b6a623fa26b2cb7b305e9d
barraIhsan <57800056+barraIhsan@users.noreply.github.com>
2024-06-03 15:08:27 +0700

    feat: vertically align Title component at bottom

--- src/components/Title.svelte
+++ src/components/Title.svelte
@@ -2,7 +2,7 @@
 	export let title, desc;
 </script>
 
-<div class="flex flex-col xl:flex-row xl:items-center gap-10 xl:gap-5">
+<div class="flex flex-col xl:flex-row xl:items-end gap-10 xl:gap-5">
 	<h1 class="text-dark font-black text-5xl xs:text-6xl min-w-72">{title}</h1>
 	<img src="/img/paw-hero.svg" alt="Cat Paw" class="w-36 md:w-52 mr-20">
 	<p class="text-lg xs:text-xl">{desc}</p>
