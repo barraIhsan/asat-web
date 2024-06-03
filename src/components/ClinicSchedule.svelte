8b7c72905d33f1aca9249b923d2b75f4ca9e9077
barraIhsan <57800056+barraIhsan@users.noreply.github.com>
2024-06-03 20:11:09 +0700

    fix: make the ClinicSchedule

--- src/components/ClinicSchedule.svelte
+++ src/components/ClinicSchedule.svelte
@@ -2,7 +2,7 @@
 	export let doctorData;
 </script>
 
-<div class="grid grid-cols-1 lg:grid-cols-2 gap-20">
+<div class="grid grid-cols-1 lg:grid-cols-2 gap-14 mx-auto sm:mx-0">
 	{#each doctorData as doctor}
 		<div class="flex flex-col sm:flex-row gap-3 text-dark text-center sm:text-left w-fit sm:w-full">
 			<img src="{doctor.imgSrc}" alt="{doctor.name}" class="size-72 sm:size-48 xl:size-56 2xl:size-72 rounded-full object-cover">
